78d2c5898ed5a2e6cf86c80c7ad491d0110bea2b
ConstantineTU <8kozmo8@gmail.com>
2021-09-04 10:44:58 +0500

    refactor: optimisation code

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -4,87 +4,79 @@ class TicTacToe {
             [null, null, null],
             [null, null, null],
             [null, null, null]
-        ];
-        this.currentPlayerSymbol = 'x';
-        this.winner = null;
+        ]
+        this.currentPlayerSymbol = 'x'
+        this.winner = null
     }
 
     getCurrentPlayerSymbol() {
-        return this.currentPlayerSymbol;
+        return this.currentPlayerSymbol
     }
 
     nextTurn(r, c) {
-        let z = this.currentPlayerSymbol;
+        let player = this.currentPlayerSymbol
 
         if (this.field[r][c] === null) {
-            this.field[r][c] = z;
-            if (this.field[0][c] == z && this.field[1][c] == z && this.field[2][c] == z) {
-                this.winner = z;
+            this.field[r][c] = player
+            if (this.field[0][c] == player && this.field[1][c] == player && this.field[2][c] == player) {
+                this.winner = player
             }
-            if (this.field[r][0] == z && this.field[r][1] == z && this.field[r][2] == z) {
-                this.winner = z;
+            if (this.field[r][0] == player && this.field[r][1] == player && this.field[r][2] == player) {
+                this.winner = player
             }
-
-            if (r == 1 && c == 1) {
-                if ((this.field[0][0] == z && this.field[1][1] == z && this.field[2][2] == z) ||
-                    (this.field[2][0] == z && this.field[1][1] == z && this.field[0][2] == z)) {
-                    this.winner = z;
-                }
+            if ((this.field[0][0] == player && this.field[1][1] == player && this.field[2][2] == player) ||
+                (this.field[2][0] == player && this.field[1][1] == player && this.field[0][2] == player)) {
+                this.winner = player
             }
-            if ((r == 2 && c == 0) || (r == 0 && c == 2)) {
-                if (this.field[2][0] == z && this.field[1][1] == z && this.field[0][2] == z) {
-                    this.winner = z;
-                }
+            if (this.field[2][0] == player && this.field[1][1] == player && this.field[0][2] == player) {
+                this.winner = player
             }
-            if ((r == 0 && c == 0) || (r == 2 && c == 2)) {
-                if (this.field[0][0] == z && this.field[1][1] == z && this.field[2][2] == z) {
-                    this.winner = z;
-                }
+            if (this.field[0][0] == player && this.field[1][1] == player && this.field[2][2] == player) {
+                this.winner = player
             }
-
             if (this.currentPlayerSymbol == 'x') {
-                this.currentPlayerSymbol = 'o';
+                this.currentPlayerSymbol = 'o'
             } else {
-                this.currentPlayerSymbol = 'x';
+                this.currentPlayerSymbol = 'x'
             }
         }
     }
 
     isFinished() {
-        if (this.winner != null || this.noMoreTurns()) {
-            return true;
-        } else {
-            return false;
+        if (this.winner || this.noMoreTurns()) {
+            return true
         }
+        return false
+
     }
 
     getWinner() {
-        return this.winner;
+        return this.winner
     }
 
     noMoreTurns() {
-        let result = true;
+        let result = true
         for (let x in this.field) {
             for (let y in this.field[x]) {
                 if (this.field[x][y] === null) {
-                    result = false;
+                    result = false
                 }
             }
         }
-        return result;
+        return result
     }
 
     isDraw() {
         if (this.winner === null && this.noMoreTurns()) {
-            return true;
+            return true
         } else {
-            return false;
+            return false
         }
     }
 
-    getFieldValue(rowIndex, colIndex) {
-        return this.field[rowIndex][colIndex];
+    getFieldValue(r, c) {
+        return this.field[r][c]
     }
 }
 
-module.exports = TicTacToe;
+module.exports = TicTacToe
